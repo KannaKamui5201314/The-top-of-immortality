c7fff0bd6ca5f80644b56f037989465f8c411ac7
Zilan <786171594@qq.com>
2024-11-02 22:14:57 +0800

    修复地界不更新问题

--- Assets/Scripts/UI/Panel.cs
+++ Assets/Scripts/UI/Panel.cs
@@ -9,6 +9,7 @@ using UnityEngine.UI;
 public class Panel : MonoBehaviour
 {
     TextMeshProUGUI Boundary;
+    private int oldBoundary;
     Button HeadPortrait;
     TextMeshProUGUI realm;
     RectTransform Health;
@@ -19,12 +20,13 @@ public class Panel : MonoBehaviour
     void Start()
     {
         Boundary = transform.Find("Boundary").GetComponent<TextMeshProUGUI>();
+        oldBoundary = Global.Boundary;
         HeadPortrait = transform.Find("HeadPortrait").GetComponent<Button>();
         realm = transform.Find("Realm").GetComponent<TextMeshProUGUI>();
         Health = (RectTransform)transform.Find("Panel_Health").Find("Health");
         HealthText = transform.Find("Panel_Health").GetComponentInChildren<TextMeshProUGUI>();
 
-        Boundary.text = "第" + Global.Boundary.ToString() + "界";
+        Boundary.text = "第" + oldBoundary.ToString() + "界";
         HeadPortrait.onClick.AddListener(OnHeadPortraitClick);
         realm.text = Realm.info[Player.Realm - 1];
         // 修改sizeDelta的x值来改变宽度，y值保持不变
@@ -45,6 +47,10 @@ public class Panel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (oldBoundary != Global.Boundary)
+        {
+            oldBoundary = Global.Boundary;
+            Boundary.text = "第" + oldBoundary.ToString() + "界";
+        }
     }
 }
